247975b993570bc1959667707da9ade918ea38ec
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-09 21:58:26 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   index = 0
   array.each do |element|
-    new_array [0] = element ** 2
+    new_array [index] = element ** 2
     index += 1
   end
   return new_array
