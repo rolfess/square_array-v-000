2f2c63a95a1e61467c40c0cf1036f8c11f8b5f3a
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-09 21:55:58 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   array.each do |element|
     new_array = element ** 2
-    new_array = += new_array
+    new_array += new_array
   end
   return new_array
 end
